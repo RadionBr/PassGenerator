e3c2f43d36450f1524c8d6ded1d54752475b9770
Radion <116784768+RadionBr@users.noreply.github.com>
2024-10-04 09:09:17 +0300

    Update NikPassGen.py

--- NikPassGen.py
+++ NikPassGen.py
@@ -2,22 +2,31 @@ import random
 import string
 
 
-def generate_passwords(nickname, count=1000000):
+def generate_passwords(nickname, count=1000000000, block_size=1000000, file_name="passwords.txt"):
     characters = string.ascii_letters + string.digits + string.punctuation
 
-    for _ in range(count):
-        # Случайная мутация никнейма (изменение регистра букв)
-        mutated_nickname = ''.join(random.choice([char.lower(), char.upper()]) for char in nickname)
+    # Открываем файл на запись в режиме добавления
+    with open(file_name, "a") as file:
+        for i in range(count):
+            # Случайная мутация никнейма (изменение регистра букв)
+            mutated_nickname = ''.join(random.choice([char.lower(), char.upper()]) for char in nickname)
 
-        # Добавление случайных символов в начало и конец
-        prefix = ''.join(random.choices(characters, k=random.randint(1, 3)))
-        suffix = ''.join(random.choices(characters, k=random.randint(1, 3)))
+            # Добавление случайных символов в начало и конец
+            prefix = ''.join(random.choices(characters, k=random.randint(1, 3)))
+            suffix = ''.join(random.choices(characters, k=random.randint(1, 3)))
 
-        # Генерация пароля с ником в середине
-        password = prefix + mutated_nickname + suffix
+            # Генерация пароля с ником в середине
+            password = prefix + mutated_nickname + suffix
 
-        # Выводим пароль сразу
-        print(password)
+            # Выводим пароль в консоль
+            print(password)
+
+            # Записываем пароль в файл
+            file.write(password + "\n")
+
+            # Опционально: выводим информацию о прогрессе каждые block_size паролей
+            if (i + 1) % block_size == 0:
+                print(f"{i + 1} паролей сгенерировано и записано в файл...")
 
 
 # Пример использования:
